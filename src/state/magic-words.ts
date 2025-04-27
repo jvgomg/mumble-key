3d0c24d75f0a7c671692022cd6f3dfc136c198ba
James Greenaway <james@cable.tech>
2025-04-27 23:43:08 +0100

    tidy up LCG

--- src/state/magic-words.ts
+++ src/state/magic-words.ts
@@ -10,22 +10,8 @@ import { MAGIC_WORDS_PHRASE_LENGTH, MagicWords } from "./domain"
  */
 
 export const getMagicWords = async (seed: number): Promise<MagicWords> => {
-  const phaseLength = MAGIC_WORDS_PHRASE_LENGTH
   const words = await getWordList()
-
-  // LCG parameters (using common values)
-  const m = words.length // modulus (using word list length)
-  const a = 1597 // multiplier
-  const c = 51749 // increment
-
-  // Generate sequence of indices using LCG
-  const indices = Array.from({ length: phaseLength }, (_, i) => {
-    // X_(n+1) = (a * X_n + c) mod m
-    const index = Math.abs((a * (seed + i) + c) % m)
-    return index
-  })
-
-  const magicWords = indices.map((index) => words[index])
+  const magicWords = shuffle(words, seed, MAGIC_WORDS_PHRASE_LENGTH)
   return magicWords as MagicWords
 }
 
@@ -35,9 +21,26 @@ export const getMagicWords = async (seed: number): Promise<MagicWords> => {
 
 const getWordList = cache(async (): Promise<string[]> => {
   const lines = await readFile(
-    join(process.cwd(), "src/magic-words.txt"),
+    join(process.cwd(), "src/magic_words.txt"),
     "utf-8",
   )
   const words = lines.split("\n").filter(Boolean)
   return words
 })
+
+const shuffle = (words: string[], seed: number, phaseLength: number) => {
+  // LCG parameters (using common values)
+  const m = words.length // modulus (using word list length)
+  const a = 1597 // multiplier
+  const c = 51749 // increment
+
+  // Generate sequence of indices using LCG
+  const indices = Array.from({ length: phaseLength }, (_, i) => {
+    // X_(n+1) = (a * X_n + c) mod m
+    const index = Math.abs((a * (seed * phaseLength + i) + c) % m)
+    return index
+  })
+
+  const phrase = indices.map((index) => words[index])
+  return phrase
+}
